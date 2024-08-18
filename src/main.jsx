b19e26f5acd01d0df3fe61dea7ea32610fa87908
IEEEMOSTAFA <skmostafa8888@gmail.com>
2024-08-18 09:16:06 +0600

    React Router Learning

--- src/main.jsx
+++ src/main.jsx
@@ -4,29 +4,42 @@
 
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
+import App from './App.jsx'
+import "./index.css";
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import "./index.css";
+import Home from "./Home/Home.jsx";
+import Header from "./Header/Header.jsx";
+import About from "./comonents/About/About.jsx";
+import Contact from "./comonents/Contact/Contact.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
-  },
-  {
-    path: "/about",
-    element: <div>I am in the about Page</div>,
-  },
-  {
-    path: "/contact",
-    element: <div>Call Me Right Now</div>,
-  },
-]);
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/about',
+        element: <About></About>
+      },
+      {
+        path:'/contact',
+        element: <Contact></Contact>
+      }
 
+    ]
+  },
+  // {
+  //   path: 'header',
+  //   element: <Header></Header>
+  // }
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    {/* <App></App> */}
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
